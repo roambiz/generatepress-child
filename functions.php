f0d529e90542ae1cf231591b9ff9211d6d8959c3
Singa <47083463+roambiz@users.noreply.github.com>
2024-02-04 17:45:03 +0800

    Blank Template
    Child Theme Blank Template

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/* * *
+ * File name:           functions.php
+ * File description:    This file primarily includes references and definitions.
+ * Template:            generatepress
+ * Theme URI:           https://github.com/roambiz/generatepress_child
+ * Author:              Singa
+ * Author URI:          https://roambiz.com/
+ * License:             GNU General Public License v3.0 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * Note: 
+ * @link https://github.com/tomusborne/generatepress
+ * @package GeneratePressChild
+ * @since GeneratePress 3.3.1
+ * * *-------------------------------------------------------------------------------------------------------------- */
+
+
+ # ===================================
+ # Main Functionality and Definitions
+ # ===================================
+ 
+// Referencing default functionality functions within the child theme

963c89c7177d82b9aa063b624a24fc13d9f62cee
Singa <47083463+roambiz@users.noreply.github.com>
2024-02-06 20:48:56 +0800

    Update gp-theme-supp-feats.php

--- inc/gp-theme-supp-feats.php
+++ inc/gp-theme-supp-feats.php
@@ -6,8 +6,6 @@
  * 
  * */
 
- namespace GeneratePressChild;
-
 // This will prevent the default comments template from being displayed in the GeneratePress theme.
 add_action( 'wp', function() {
     remove_action( 'generate_after_do_template_part', 'generate_do_comments_template', 15 );

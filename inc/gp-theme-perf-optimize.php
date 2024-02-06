5721ead82e2a925cea27b25ff8da635346ba4a34
Singa <47083463+roambiz@users.noreply.github.com>
2024-02-06 22:56:35 +0800

    使用命名空间

--- inc/gp-theme-perf-optimize.php
+++ inc/gp-theme-perf-optimize.php
@@ -9,37 +9,48 @@
  * @domain: ---
  * 
  * */
+namespace GeneratePressChild;
 
-# Preload the LCP resources with a high fetch priority to ensure they start loading with the stylesheet.
-function add_preload_tags_for_lcp_resources() {
-    $lcp_link_tags = array(
-        //"<link rel='preload' fetchpriority='high' as='image' href='#' type='image/webp'>",
-        //"<link rel='preload' fetchpriority='high' as='font' href='#' type='font/woff2' crossorigin='anonymous'>",
-        //"<link rel='preload' fetchpriority='low' as='video' href='#' type='video/mp4'>",
-    );
-
-    // Output preload link tags
-    foreach ($lcp_link_tags as $tag) {
-        echo $tag;
+class Html {
+    public static function init() {
+        $html = new self();
+        add_action( 'wp_enqueue_scripts', array( $html, 'add_tags' ) );
     }
-}
 
-# Remove core block styles from the front-end
-function remove_core_block_non_critical_styles() {
-    global $wp_styles;
+    public static function add_tags() {
+        $lcp_link_tags = array(
+            //"<link rel='preload' fetchpriority='high' as='image' href='#' type='image/webp'>",
+            //"<link rel='preload' fetchpriority='high' as='font' href='#' type='font/woff2' crossorigin='anonymous'>",
+            //"<link rel='preload' fetchpriority='low' as='video' href='#' type='video/mp4'>",
+        );
 
-    // Loop through the style queue
-    foreach ( $wp_styles->queue as $key => $handle ) {
-        // Check if the handle starts with 'wp-block-'
-        if ( strpos( $handle, 'wp-block-' ) === 0 ) {
-            // Dequeue the style if it is a core block style
-            wp_dequeue_style( $handle );
+        // Output preload link tags
+        foreach ($lcp_link_tags as $tag) {
+            echo $tag;
         }
     }
 }
-// Hook the function to the 'wp_enqueue_scripts' action
-add_action( 'wp_enqueue_scripts', 'remove_core_block_non_critical_styles' );
 
+class Wordpress {
+    // Hook the function to the 'wp_enqueue_scripts' action
+    public static function init() {
+        $wordpress = new self();
+        add_action( 'wp_enqueue_scripts', array( $wordpress, 'remove_non_critical_css' ) );
+    }
+
+    public function remove_non_critical_css() {
+        global $wp_styles;
+
+        // Loop through the style queue
+        foreach ( $wp_styles->queue as $key => $handle ) {
+            // Check if the handle starts with 'wp-block-'
+            if ( strpos( $handle, 'wp-block-' ) === 0 ) {
+            // Dequeue the style if it is a core block style
+                wp_dequeue_style( $handle );
+            }
+        }
+    }
+}
 
 // END //
 ?>

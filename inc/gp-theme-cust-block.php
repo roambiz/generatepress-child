f8e82a8a1772b33df5ffaf0407c7c9dd188c7347
Singa <47083463+roambiz@users.noreply.github.com>
2024-02-08 23:52:37 +0800

    优化结构，添加逻辑判断

--- inc/gp-theme-cust-block.php
+++ inc/gp-theme-cust-block.php
@@ -10,6 +10,7 @@
  * @domain: ---
  * 
  * */
+<?php
 namespace GeneratePressChild;
 
 class Block {
@@ -18,83 +19,126 @@ class Block {
     }
 
     public function core_block_types( $allowed_block_types, $editor_context ) {
+        // Define an empty array by default
+        $generateblocks_block_types = array();
+
+        // Define an array for post
+        $cust_core_block_types_for_post = array(
+            // TEXT
+            'core/paragraph',
+            'core/list',
+            'core/code',
+            'core/preformatted',
+            'core/table',
+            'core/footnotes',
+            // MEDIA
+            'core/gallery',
+            'core/file',
+            // DESIGN
+            'core/nextpage',
+            'core/spacer',
+            // WIDGETS
+            'core/html',
+            'core/latest-posts',
+            'core/search',
+            'core/shortcode',
+            'core/social-link',
+            'core/social-links',
+            'core/tag-cloud',
+        );
+
+        // Define an array for page
+        $cust_core_block_types_for_page = array(
+            // TEXT
+            'core/paragraph',
+            'core/list',
+            'core/code',
+            'core/preformatted',
+            'core/table',
+            'core/footnotes',
+            // MEDIA
+            'core/gallery',
+            'core/file',
+            // DESIGN
+            'core/nextpage',
+            'core/spacer',
+            // WIDGETS
+            'core/html',
+            'core/search',
+            'core/shortcode',
+        );
+
+        // Define an array for seo-project
+        $cust_core_block_types_for_seo_project = array(
+            // TEXT
+            'core/paragraph',
+            'core/list',
+            'core/code',
+            'core/preformatted',
+            'core/table',
+            'core/footnotes',
+            // MEDIA
+            'core/gallery',
+            'core/file',
+            // DESIGN
+            'core/nextpage',
+            'core/spacer',
+            // WIDGETS
+            'core/html',
+            'core/search',
+            'core/shortcode',
+            'core/social-link',
+            'core/social-links',
+            'core/tag-cloud',
+        );
+
+        // Check if GenerateBlocks plugin is active
+        if ( is_plugin_active( 'generateblocks/generateblocks.php' ) ) {
+            // GenerateBlocks plugin is active, allow GenerateBlocks block types
+            $generateblocks_block_types = array(
+                'generateblocks/container',
+                'generateblocks/grid',
+                'generateblocks/query-loop',
+                'generateblocks/button-container',
+                'generateblocks/headline',
+                'generateblocks/button',
+                'generateblocks/image',
+                'generatepress/dynamic-content',
+                'generatepress/dynamic-image',
+                'contact-form-7/contact-form-selector',
+            );
+        }
+
         // If the post type is the default post type ('post')
         if ( 'post' === $editor_context->post->post_type ) {
             // Allow specified block types
-            return array(
-                // TEXT
-                'core/paragraph',
-                'core/list',
-                'core/code',
-                'core/preformatted',
-                'core/table',
-                'core/footnotes',
-                // MEDIA
-                'core/gallery',
-                'core/file',
-                // DESIGN
-                'core/nextpage',
-                'core/spacer',
-                // WIDGETS
-                'core/html',
-                'core/latest-posts',
-                'core/search',
-                'core/shortcode',
-                'core/social-link',
-                'core/social-links',
-                'core/tag-cloud',
+            return array_merge(
+                // GENERATEBLOCKS
+                $generateblocks_block_types,
+                // POST
+                $cust_core_block_types_for_post
             );
         }
 
         // If the post type is the default page type ('page')
         if ( 'page' === $editor_context->post->post_type ) {
             // Allow specified block types
-            return array(
-                // TEXT
-                'core/paragraph',
-                'core/list',
-                'core/code',
-                'core/preformatted',
-                'core/table',
-                'core/footnotes',
-                // MEDIA
-                'core/gallery',
-                'core/file',
-                // DESIGN
-                'core/nextpage',
-                'core/spacer',
-                // WIDGETS
-                'core/html',
-                'core/search',
-                'core/shortcode',
-
+            return array_merge(
+                // GENERATEBLOCKS
+                $generateblocks_block_types,
+                // PAGE
+                $cust_core_block_types_for_page
             );
         }
 
         // If the post type is the custom post type 'seo-project'
         if ( 'seo-project' === $editor_context->post->post_type ) {
             // Allow specified block types
-            return array(
-                // TEXT
-                'core/paragraph',
-                'core/list',
-                'core/code',
-                'core/preformatted',
-                'core/table',
-                'core/footnotes',
-                // MEDIA
-                'core/gallery',
-                'core/file',
-                // DESIGN
-                'core/nextpage',
-                'core/spacer',
-                // WIDGETS
-                'core/html',
-                'core/search',
-                'core/shortcode',
-                'core/social-link',
-                'core/social-links',
-                'core/tag-cloud',
+            return array_merge(
+                // GENERATEBLOCKS
+                $generateblocks_block_types,
+                // SEO-PROJECT
+                $cust_core_block_types_for_seo_project
             );
         }
 
@@ -102,3 +146,5 @@ class Block {
         return $allowed_block_types;
     }
 }
+
+// 
